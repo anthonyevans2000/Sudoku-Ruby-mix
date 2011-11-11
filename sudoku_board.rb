238030ed6a986b828713c918d3423cdf7ce346aa
Anthony Evans <anthonyevans2000@gmail.com>
2011-11-11 23:29:58 +1100

    Finished board array generation and printing, working on value availability checking

--- sudoku_board.rb
+++ sudoku_board.rb
@@ -1,19 +1,28 @@
 #A class to define a sudoku board and methods to operate on it
 
-class SudokuBoard(
-  attr_accessor :board 
+class SudokuBoard
+  attr_accessor :board, :print, :valid_values 
   
   
-
   def initialize(size)
     @board = []
-    @board = (size*3).times{@board << Array.new(size*3)}
+    (size*3).times{@board << Array.new(size*3)}
     values = (1..(size*3)).to_a
-    @valid_values = (size*3).times{@valid_values << values.dup}    
+
+    @valid_values = []
+    (size*3).times{@valid_values << values.dup}    
   end
 
- # def return_valid_value(x,y)
-    
+  def print
+    @board.each do |line|
+      puts line.join
+    end
+  
+  end
 
+#  def return_valid_value(x,y)
+#    board[x].each{
+    
+end
       
   
