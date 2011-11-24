0d8de88bade13b8451d34bfab29da048e8ed69cf
Anthony Evans <anthonyevans2000@gmail.com>
2011-11-24 23:50:33 +1100

    Starting work on logic to remove numbers from a position, allowing creation of a sparse game board

---
+++ sudoku_cleaner.rb
@@ -0,0 +1,15 @@
+#A class to take a full Sudoku-type board and apply removal techniques in order to generate puzzle boards.
+require 'sudoku_board.rb'
+
+class Point_Remover
+  
+  attr_accessor :check_line
+  
+ # def initialize(board)
+ # end
+
+  def check_lines(x,y)
+  
+  end
+
+end
